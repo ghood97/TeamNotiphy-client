1340d08b530a183f180a543d4ce9e2eb9712ce23
Griffin Hood <griffinhood@verizon.net>
2019-11-29 16:34:35 -0500

    Position Footer on bottom of viewport

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
+const footerStyle = {
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  right: 0
+}
+
 const Footer = (props) => {
   return (
-    <p className="text-center">A website by Griffin Hood</p>
+    <p style={footerStyle} className="text-center">A website by Griffin Hood</p>
   )
 }
 
