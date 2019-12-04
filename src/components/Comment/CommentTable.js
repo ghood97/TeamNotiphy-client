a2f02ad02a69e40070d3b49a7a8d2bdbb96fd165
Griffin Hood <griffinhood@verizon.net>
2019-12-04 09:29:30 -0500

    Add created_at date to somments table

--- src/components/Comment/CommentTable.js
+++ src/components/Comment/CommentTable.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { withRouter } from 'react-router-dom'
 import { Table, Button, ButtonGroup } from 'react-bootstrap'
 import './CommentTable.scss'
+import formatDate from '../../formatDate'
 
 const CommentTable = (props) => {
   const commentJsx = props.comments.map(x => (
     <tr key={x.id}>
       <td className="font-weight-bold tableRow text-wrap text-break col-6">{x.text}</td>
       <td className="font-weight-bold tableRow text-right col-4">{x.user.email}</td>
+      <td className="font-weight-bold tableRow text-right col-4">{formatDate(new Date(x.created_at))}</td>
       {props.user.id === x.user.id
         ? <td className="text-right col-2">
           <ButtonGroup vertical>
