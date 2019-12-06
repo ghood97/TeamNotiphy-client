630c09aeace60310df286eadb1417011b5f3a694
Griffin Hood <griffinhood@verizon.net>
2019-12-06 16:50:05 -0500

    Change history.push in sign-in

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -33,7 +33,7 @@ class SignIn extends Component {
         message: messages.signInSuccess,
         variant: 'success'
       }))
-      .then(() => history.push('/')
+      .then(() => history.push('/'))
       .catch(() => {
         this.setState({ email: '', password: '' })
         alert({
